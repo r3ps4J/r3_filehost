2f870f47654b9e1bf5530770cc479de40540f923
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2024-03-20 14:37:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# r3_filehost
+Simple resource allowing you to host files directly on FXServer. Meant to be used with phone scripts that upload image/video/audio files.
---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+-- FX information
+fx_version "cerulean"
+game "common"
+
+-- Resource information
+name "r3_filehost"
+version "1.0.0"
+description "Simple resource allowing you to host files directly on FXServer."
+author "r3ps4J"
+license "LGPL-3.0-or-later"
+repository "https://github.com/r3ps4J/r3_filehost"
+
+-- Manifest
+server_script "dist/index.js"
