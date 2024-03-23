a2856a0553f20c0f9858c778813a03a6ff631758
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2024-03-23 20:30:52 +0100

    Add assets folder to pack script

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,5 +1,5 @@
 mkdir -p ./temp/r3_filehost
 cp ./{fxmanifest.lua,LICENSE,README.md} ./temp/r3_filehost
-cp -r ./dist ./temp/r3_filehost
+cp -r ./{dist,assets} ./temp/r3_filehost
 cd ./temp && zip -r ../r3_filehost.zip ./r3_filehost
 cd .. && rm -rf ./temp
