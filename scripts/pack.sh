494d647b04c1f532aca77cec91eaf713e15dded0
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2024-04-10 11:23:01 +0200

    Add comments to pack.sh

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Create temporary directory
 mkdir -p ./temp/r3_filehost
+
+# Copy files to include in pack
 cp ./{fxmanifest.lua,LICENSE,README.md} ./temp/r3_filehost
 cp -r ./{dist,assets} ./temp/r3_filehost
+
+# Create zip and remove temp directory
 cd ./temp && zip -r ../r3_filehost.zip ./r3_filehost
 cd .. && rm -rf ./temp
